b3550b5271d221b07f8fd02269d54441b829c15c
DaveEveritt <deveritt@innotts.co.uk>
2017-03-11 12:50:36 +0000

    tidied and removed redundant code

--- js/square-test.js
+++ js/square-test.js
@@ -1,34 +1,34 @@
 "use strict";
 
 (function(){
-  // Check: the right number of integers for an n x n matrix?
+  // Removes non-integers and checks for an n x n matrix?
   
-  let dataString = "1 2 3 4.5 'hello' 5 6 7 8 9 10 11 12 13 -14 15 16";
+  let dataString = "1 2 3 4 4.5 'hello' 5 6 7 8 9 10 11 12 13 -14 15 16";
   let outputToPrint = `<p>Original string:<br>${dataString}</p>`
 
-  // splits space-separated string into an array of numbers:
+  // Splits space-separated string into an array of numbers:
   let dataArray = dataString.split(' ').map(Number);
-  // removes non-numbers:
+
+  // Removes non-numbers:
   dataArray = [...new Set(dataArray.filter(Boolean))]
+  outputToPrint += `<p>Should contain only numbers:<br>${dataArray.map(num => num +0)}</p>`;
 
 /* 1. remove floats and negative numbers */
   // removes floats:
   dataArray = dataArray.map(Math.floor);
   // TODO: delete floats instead of math.floor
-  
-  // add 1 to all array elements to check for NaN
-  outputToPrint += `<p>Test for non-numbers by adding zero to all elements:<br>${dataArray.map(num => num +0)}</p>`;
 
   // removes negative numbers:
   dataArray = dataArray.map(Math.abs);
 
-  outputToPrint += `<p>Should be no negative numbers:<br>${dataArray}</p>`;
+  outputToPrint += `<p>Should be no floats or negative numbers:<br>${dataArray}</p>`;
   // TODO: retain negatives for balanced squares e.g. 1, -1, 2, -2 etc.?
 /* END 1 */
 
 
 /* 2. remove duplicates and get length */
   let dataArrayUndup = [...new Set(dataArray)];
+  outputToPrint += `<p>Should be no dupicated numbers:<br>${dataArrayUndup}</p>`;
 /* END 2 */
 
 
@@ -37,7 +37,7 @@
   let order = Math.sqrt(dataArrayUndup.length);
   let isInteger = (dataArrayUndup.length % order == 0);
   // return true pr false here
-  outputToPrint += `Square root of array length ${dataArrayUndup.length} is: ${order}<br>If this is this an integer (${isInteger}) then this is a square (n x n) array: ${isInteger}</p>`;
+  outputToPrint += `Square root of array length (${dataArrayUndup.length}) is: ${order}<br>If this is this an integer (${isInteger}) then this is a square (n x n) array: ${isInteger}</p>`;
 /* END 3 */
 
 
